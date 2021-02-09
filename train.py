1f8a0e5fbca88f8d6a32a441aa7a9b16be114b25
solmarinioturdera <solmarinoturdera@gmail.com>
2021-02-09 10:26:37 -0300

    commited

--- train.py
+++ train.py
@@ -1,20 +1,42 @@
 from __future__ import print_function
-import tensorflow as tf
 import os
-# import torch
-# import torch.optim as optim
-# import torch.backends.cudnn as cudnn
-# import argparse
-# import torch.utils.data as data
-# from data import AnnotationTransform, VOCDetection, detection_collate, preproc, cfg
-# from layers.modules import MultiBoxLoss
-# from layers.functions.prior_box import PriorBox
-# import time
-# import datetime
-# import math
-# from models.faceboxes import FaceBoxes
+import argparse
+from data import AnnotationTransform, VOCDetection, detection_collate, preproc, cfg
+from models.faceboxes import FaceBoxes
 
-if tf.test.gpu_device_name():
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
-else:
-    print("Please install GPU version of TF")
+parser = argparse.ArgumentParser(description='FaceBoxes Training')
+parser.add_argument('--training_dataset', default='./data/WIDER_FACE', help='Training dataset directory')
+parser.add_argument('-b', '--batch_size', default=32, type=int, help='Batch size for training')
+parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
+parser.add_argument('--ngpu', default=2, type=int, help='gpus')
+parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--resume_net', default=None, help='resume net for retraining')
+parser.add_argument('--resume_epoch', default=0, type=int, help='resume iter for retraining')
+parser.add_argument('-max', '--max_epoch', default=300, type=int, help='max epoch for retraining')
+parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
+parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
+parser.add_argument('--save_folder', default='./weights/', help='Location to save checkpoint models')
+args = parser.parse_args()
+
+if not os.path.exists(args.save_folder):
+    os.mkdir(args.save_folder)
+
+img_dim = 1024 # only 1024 is supported
+rgb_mean = (104, 117, 123) # bgr order
+num_classes = 2
+num_gpu = args.ngpu
+num_workers = args.num_workers
+batch_size = args.batch_size
+momentum = args.momentum
+weight_decay = args.weight_decay
+initial_lr = args.lr
+gamma = args.gamma
+max_epoch = args.max_epoch
+training_dataset = args.training_dataset
+save_folder = args.save_folder
+gpu_train = cfg['gpu_train']
+
+net = FaceBoxes('train', img_dim, num_classes)
+print("Printing net...")
+print(net)
