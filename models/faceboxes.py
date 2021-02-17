26123a1f871f85a934544d3d8accddf14f4fe707
solmarinioturdera <solmarinoturdera@gmail.com>
2021-02-17 10:01:43 -0300

    commited

--- models/faceboxes.py
+++ models/faceboxes.py
@@ -1,66 +1,52 @@
 import tensorflow as tf
-import tensorflow.nn as nn
-import tf.keras.layers as layers
+import tensorflow.keras.layers as layers
+from tensorflow.keras import Sequential
 
-# Hola
 
-class BasicConv2d(nn.Module):
+class Basicconv2d(tf.Module):
 
     def __init__(self, in_channels, out_channels, **kwargs):
-        super(BasicConv2d, self).__init__()
-        # self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        super(Basicconv2d, self).__init__()
+        self.conv = tf.nn.conv2d(in_channels, out_channels, bias=False, **kwargs)
         self.bn = layers.BatchNormalization(out_channels, epsilon=1e-5)
-
-        # out_channels = tf.nn.conv2d(in_channels, )
 
     def __call__(self, x):
         x = self.conv(x)
         x = self.bn(x)
-        return nn.relu(x)
+        return tf.nn.relu(x)
 
 
-class CRelu(nn.Module):
+class CRelu(tf.Module):
 
   def __init__(self, in_channels, out_channels, **kwargs):
     super(CRelu, self).__init__()
-    # self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+    self.conv = tf.nn.conv2d(in_channels, out_channels, bias=False, **kwargs)
     self.bn = layers.BatchNormalization(out_channels, eps=1e-5)
 
   def __call__(self, x):
     x = self.conv(x)
     x = self.bn(x)
     x = tf.concat([x, -x], 1)
-    x = nn.relu(x)
+    x = tf.nn.relu(x)
     return x
 
-  def multibox(self, num_classes):
-    loc_layers = []
-    conf_layers = []
-    # loc_layers += [nn.Conv2d(128, 21 * 4, kernel_size=3, padding=1)]
-    # conf_layers += [nn.Conv2d(128, 21 * num_classes, kernel_size=3, padding=1)]
-    # loc_layers += [nn.Conv2d(256, 1 * 4, kernel_size=3, padding=1)]
-    # conf_layers += [nn.Conv2d(256, 1 * num_classes, kernel_size=3, padding=1)]
-    # loc_layers += [nn.Conv2d(256, 1 * 4, kernel_size=3, padding=1)]
-    # conf_layers += [nn.Conv2d(256, 1 * num_classes, kernel_size=3, padding=1)]
-    # return nn.Sequential(*loc_layers), nn.Sequential(*conf_layers)
 
-
-class Inception(nn.Module):
+class Inception(tf.Module):
 
   def __init__(self):
     super(Inception, self).__init__()
-    self.branch1x1 = BasicConv2d(128, 32, kernel_size=1, padding=0)
-    self.branch1x1_2 = BasicConv2d(128, 32, kernel_size=1, padding=0)
-    self.branch3x3_reduce = BasicConv2d(128, 24, kernel_size=1, padding=0)
-    self.branch3x3 = BasicConv2d(24, 32, kernel_size=3, padding=1)
-    self.branch3x3_reduce_2 = BasicConv2d(128, 24, kernel_size=1, padding=0)
-    self.branch3x3_2 = BasicConv2d(24, 32, kernel_size=3, padding=1)
-    self.branch3x3_3 = BasicConv2d(32, 32, kernel_size=3, padding=1)
+    self.branch1x1 = Basicconv2d(128, 32, kernel_size=1, padding=0)
+    self.branch1x1_2 = Basicconv2d(128, 32, kernel_size=1, padding=0)
+    self.branch3x3_reduce = Basicconv2d(128, 24, kernel_size=1, padding=0)
+    self.branch3x3 = Basicconv2d(24, 32, kernel_size=3, padding=1)
+    self.branch3x3_reduce_2 = Basicconv2d(128, 24, kernel_size=1, padding=0)
+    self.branch3x3_2 = Basicconv2d(24, 32, kernel_size=3, padding=1)
+    self.branch3x3_3 = Basicconv2d(32, 32, kernel_size=3, padding=1)
 
   def __call__(self, x):
     branch1x1 = self.branch1x1(x)
 
-    branch1x1_pool = nn.avg_pool2d(x, ksize=3, stride=1, padding=1)
+    branch1x1_pool = tf.nn.avg_pool2d(x, ksize=3, stride=1, padding=1)
     branch1x1_2 = self.branch1x1_2(branch1x1_pool)
 
     branch3x3_reduce = self.branch3x3_reduce(x)
@@ -74,7 +60,7 @@ class Inception(nn.Module):
     return tf.concat(outputs, 1)
 
 
-class FaceBoxes(nn.Module):
+class FaceBoxes(tf.Module):
 
   def __init__(self, phase, size, num_classes):
     super(FaceBoxes, self).__init__()
@@ -88,28 +74,45 @@ class FaceBoxes(nn.Module):
     self.inception1 = Inception()
     self.inception2 = Inception()
     self.inception3 = Inception()
-    self.conv3_1 = BasicConv2d(128, 128, kernel_size=1, stride=1, padding=0)
-    self.conv3_2 = BasicConv2d(128, 256, kernel_size=3, stride=2, padding=1)
+    self.conv3_1 = Basicconv2d(128, 128, kernel_size=1, stride=1, padding=0)
+    self.conv3_2 = Basicconv2d(128, 256, kernel_size=3, stride=2, padding=1)
 
-    self.conv4_1 = BasicConv2d(256, 128, kernel_size=1, stride=1, padding=0)
-    self.conv4_2 = BasicConv2d(128, 256, kernel_size=3, stride=2, padding=1)
+    self.conv4_1 = Basicconv2d(256, 128, kernel_size=1, stride=1, padding=0)
+    self.conv4_2 = Basicconv2d(128, 256, kernel_size=3, stride=2, padding=1)
 
-    # self.loc, self.conf = self.multibox(self.num_classes)
+    self.loc, self.conf = self.multibox(self.num_classes)
 
-    # if self.phase == 'test':
-    #     self.softmax = nn.Softmax(dim=-1)
+    if self.phase == 'test':
+        # self.softmax = tf.nn.Softmax(dim=-1)
 
-    # if self.phase == 'train':
-    #     for m in self.modules():
-    #         if isinstance(m, nn.Conv2d):
-    #             if m.bias is not None:
-    #                 nn.init.xavier_normal_(m.weight.data)
-    #                 m.bias.data.fill_(0.02)
-    #             else:
-    #                 m.weight.data.normal_(0, 0.01)
-    #         elif isinstance(m, nn.BatchNorm2d):
-    #             m.weight.data.fill_(1)
-    #             m.bias.data.zero_()
+    if self.phase == 'train':
+        for m in self.modules():
+            if isinstance(m, tf.nn.conv2d):
+                if m.bias is not None:
+                    tf.nn.init.xavier_normal_(m.weight.data)
+                    m.bias.data.fill_(0.02)
+                else:
+                    m.weight.data.normal_(0, 0.01)
+            elif isinstance(m, tf.nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+  def multibox(self, num_classes):
+    loc_layers = []
+    conf_layers = []
+    loc_layers += [tf.nn.conv2d(128, 21 * 4, kernel_size=3, padding=1)]
+    conf_layers += [tf.nn.conv2d(128, 21 * num_classes, kernel_size=3, padding=1)]
+    loc_layers += [tf.nn.conv2d(256, 1 * 4, kernel_size=3, padding=1)]
+    conf_layers += [tf.nn.conv2d(256, 1 * num_classes, kernel_size=3, padding=1)]
+    loc_layers += [tf.nn.conv2d(256, 1 * 4, kernel_size=3, padding=1)]
+    conf_layers += [tf.nn.conv2d(256, 1 * num_classes, kernel_size=3, padding=1)]
+
+    model = Sequential()
+    model.add(*loc_layers)
+    model.add(*conf_layers)
+
+    return model
+    # return tf.nn.Sequential(*loc_layers), tf.nn.Sequential(*conf_layers)
 
   def __call__(self, x):
 
